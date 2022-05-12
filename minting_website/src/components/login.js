77a03f0bc231db520b0db41c16529dd2ecc1aa42
David Wang <david.wang5766@gmail.com>
2022-05-11 21:32:14 -0700

    fixed login to MetaMask border

--- minting_website/src/components/login.js
+++ minting_website/src/components/login.js
@@ -3,7 +3,7 @@ function Login() {
     const { authenticate, isAuthenticated } = useMoralis();
     return(
         <div className="flex w-screen h-screen justify-center items-center">
-            <button onClick={authenticate} class   Name = "border-[1px] p-2 border-black">Login to MetaMask</button>
+            <button onClick={authenticate} className = "border-[1px] p-2 border-black">Login to MetaMask</button>
         </div>
     )
 }
