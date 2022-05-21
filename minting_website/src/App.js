18f3425fb5e38a2a62068b9669b1fecdc31a524b
David Wang <david.wang5766@gmail.com>
2022-05-21 14:52:04 -0700

    added minter.js w/ call to smart contract

--- minting_website/src/App.js
+++ minting_website/src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/login';
+import Minter from './components/minter';
+import {Route, Routes, BrowserRouter} from 'react-router-dom';
 function App() {
   return (
     <div>
-      <Login/>
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/" element = {<Login/>}/>
+          <Route path = "/minter" element = {<Minter/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
